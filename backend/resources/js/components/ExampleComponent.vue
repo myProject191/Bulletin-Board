6844beb4e825cbc6aa0d04dec6e8f7ca383a67a3
myProject191 <mwam011257@gmail.com>
2021-09-13 17:47:09 +0900

    installed vue

---
+++ backend/resources/js/components/ExampleComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="content">
+        <h1>File Upload</h1>
+        <p><input type="file"></p>
+        <button>アップロード</button>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+.content{
+    margin:5em;
+}
+</style>
